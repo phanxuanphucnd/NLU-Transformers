f48003ba8bf8da868bef8124f2cb71514f4bd66b
phanxuanphucnd <phanxuanphucnd@gmail.com>
2021-09-16 16:33:39 +0700

    update

--- arizona/nlu/learners/joint/joint_coberta_learner.py
+++ arizona/nlu/learners/joint/joint_coberta_learner.py
@@ -7,14 +7,15 @@ import logging
 import numpy as np
 
 from tqdm import tqdm, trange
-from transformers import PreTrainedTokenizerBase
 from transformers import AdamW, get_linear_schedule_with_warmup
 from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
 
+from arizona.utils import set_seed
+from arizona.utils import compute_metrics
 from arizona.utils import get_from_registry
 from arizona.nlu.models.joint import JointCoBERTa
+from arizona.utils import CONFIGS_REGISTRY, MODELS_REGISTRY
 from arizona.nlu.datasets.joint_dataset import JointNLUDataset
-from arizona.utils import CONFIGS_REGISTRY, MODELS_REGISTRY, TOKENIZERS_REGISTRY
 
 logger = logging.getLogger(__name__)
 
@@ -24,19 +25,21 @@ class JointCoBERTaLearner():
         model: JointCoBERTa=None, 
         model_name_or_path: str=None,
         model_type: str='coberta',
-        tokenizer: PreTrainedTokenizerBase=None, 
         device: str=None,
+        seed: int=123, 
         **kwargs
     ):
         super(JointCoBERTaLearner, self).__init__()
+        
+        set_seed(seed)
         
         self.dropout = kwargs.pop('dropout', 0.1)
         self.use_crf = kwargs.pop('use_crf', True)
         self.ignore_index = kwargs.pop('ignore_index', 0)
         self.tag_loss_coef = kwargs.pop('tag_loss_coef', 1.0)
+        self.pad_token_label_id = self.ignore_index
 
         self.model = model
-        self.tokenizer = tokenizer
         self.model_type = model_type
         self.model_name_or_path = model_name_or_path
         self.config_class = get_from_registry(model_type, CONFIGS_REGISTRY)
@@ -69,8 +72,8 @@ class JointCoBERTaLearner():
         logging_steps: int=200,
         save_steps: int=1000,
         view_model: bool=True, 
-        save_dir: str='./model',
-        model_name: str='coberta-mini',
+        model_dir: str='./model',
+        model_name: str='coberta-mini.nlu',
         **kwargs
     ):
         logger.info(f"Dataset Info")
@@ -122,7 +125,7 @@ class JointCoBERTaLearner():
         optimizer = AdamW(optimizer_grouped__parameters, lr=learning_rate, eps=adam_epsilon)
         scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=warmup_steps, num_training_steps=t_total)
 
-        logger.info(f"➖➖➖➖➖ RUNNING TRAINING ➖➖➖➖➖")
+        logger.info(f"➖➖➖➖➖ Running training ➖➖➖➖➖")
         logger.info(f" Num examples = {len(train_dataset)}")
         logger.info(f" Num epochs = {n_epochs}")
         logger.info(f" Total train batch size = {train_batch_size}")
@@ -172,7 +175,7 @@ class JointCoBERTaLearner():
                         self.evaluate(test_dataset, eval_batch_size)
                     
                     if save_steps > 0 and global_step % save_steps == 0:
-                        self.save_model()
+                        self.save_model(model_dir, model_name)
 
             if 0 < max_steps < global_step:
                 epoch_iterator.close()
@@ -184,7 +187,7 @@ class JointCoBERTaLearner():
         eval_sampler = SequentialSampler(dataset)
         eval_dataloader = DataLoader(dataset, sampler=eval_sampler, batch_size=batch_size)
 
-        logger.info(f"➖➖➖➖➖ RUNNING EVALUATION ➖➖➖➖➖")
+        logger.info(f"➖➖➖➖➖ Running evaluation ➖➖➖➖➖")
         logger.info(f" Num exmaples = {len(dataset)}")
         logger.info(f" Batch size = {batch_size}")
 
@@ -249,6 +252,27 @@ class JointCoBERTaLearner():
         intent_preds = np.argmax(intent_preds)
 
         # TODO: Tag results
+        if not self.use_crf:
+            tag_preds = np.argmax(tag_preds, axis=2)
+
+        tag_label_map = {i: label for i, label in enumerate(self.tag_label_list)}
+        out_tag_label_list = [[] for _ in range(out_tag_labels_ids.shape[0])]
+        tag_preds_list = [[] for _ in range(out_tag_labels_ids.shape[0])]
+
+        for i in range(out_tag_labels_ids.shape[0]):
+            for j in range(out_tag_labels_ids.shape[1]):
+                if out_tag_labels_ids[i, j] != self.pad_token_label_id:
+                    out_tag_label_list[i].append(tag_label_map[out_tag_labels_ids[i][j]])
+                    tag_preds_list[i].append(tag_label_map[tag_preds[i][j]])
+
+        total_results = compute_metrics(intent_preds, out_intent_label_ids, tag_preds_list, out_tag_label_list)
+        results.update(total_results)
+
+        logger.info(f"➖➖➖➖➖ Evaluation results ➖➖➖➖➖")
+        for key in sorted(results.keys()):
+            logger.info(f" {key} = {str(results[key])}")
+
+        return results
 
 
     def predict(self):
@@ -257,8 +281,55 @@ class JointCoBERTaLearner():
     def process(self):
         raise NotImplementedError
 
-    def save_model(self):
-        raise NotImplementedError
+    def save_model(self, model_dir: str='./models', model_name: str='nlu-coberta-mini'):
+        model_path = os.path.join(model_dir, model_name)
+        if not os.path.exists(model_path):
+            os.makedirs(model_path)
+        
+        model_to_save = self.model.module if hasattr(self.mode, 'module') else self.model
+        model_to_save.save_pretrained(model_path)
 
-    def load_model(self):
-        raise NotImplementedError
+        # TODO: Save training arguments togethor with the trained model
+        torch.save(
+            {
+                'dropout': self.dropout,
+                'use_crf': self.use_crf,
+                'ignore_index': self.ignore_index,
+                'tag_loss_coef': self.tag_loss_coef,
+                'intent_label_list': self.intent_label_list,
+                'tag_label_list': self.tag_label_list,
+            },
+            os.path.join(model_path, 'training_args.bin')
+        )
+
+        logger.info(f"Saving model checkpoint to {model_path}")
+
+    def load_model(self, model_path: str=None):
+        if not os.path.exists(model_path):
+            raise Exception(f"Model doesn't exists or broken !")
+        
+        try:
+            checkpoint = torch.load(os.path.join(model_path, 'training_args.bin'))
+            self.dropout = checkpoint.get('dropout')
+            self.use_crf = checkpoint.get('use_crf')
+            self.ignore_index = checkpoint.get('ignore_index')
+            self.tag_loss_coef = checkpoint.get('tag_loss_coef')
+            self.intent_label_list = checkpoint.get('intent_label_list')
+            self.tag_label_list = checkpoint.get('tag_label_list')
+
+            self.model = self.model_class.from_pretrained(
+                model_path,
+                config=self.config,
+                dropout=self.dropout,
+                use_crf=self.use_crf,
+                ignore_index=self.ignore_index,
+                tag_loss_coef=self.tag_loss_coef,
+                intent_label_list=self.intent_label_list,
+                tag_label_list=self.tag_label_list
+            )
+
+            self.model.to(self.device)
+            logger.info(f"******* Model Loaded *******")
+
+        except:
+            raise Exception(f"Some model files might be missing...")
