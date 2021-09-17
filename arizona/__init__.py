340cecc962f406af2e7db9e9b61242cda85ced77
phanxuanphucnd <phanxuanphucnd@gmail.com>
2021-09-17 13:11:33 +0700

    update

--- arizona/__init__.py
+++ arizona/__init__.py
@@ -1,2 +1,8 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2021 by Phuc Phan
+
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
+                    datefmt='%m/%d/%Y %H:%M:%S',
+                    level=logging.INFO)
